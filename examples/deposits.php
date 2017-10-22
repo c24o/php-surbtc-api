72d229df69526dbb1d9b9e05067a5979853b5a8a
Carlos Guzman <caralbgm@gmail.com>
2017-10-22 12:10:39 -0500

    Addition of examples to use methods of the library.

---
+++ examples/deposits.php
@@ -0,0 +1,23 @@
+<?php
+//1. Include Surbtc library
+require_once '../Surbtc.php';
+
+//2. Create Surbtc object
+$surbtc = new SurBTC\Surbtc('api_key', 'secret_key');
+
+//2.1 activate debug mode
+//$surbtc->debug(true);
+
+//2.2 authenticate if you don't authenticate in constructor
+//$surbtc->authenticate('api_key', 'secret_key');
+
+//3. Call method to get deposits
+$response = $surbtc->deposits('cop');
+
+//4. Check response
+if(false != $response)
+  print_r($response);
+else{
+  print_r($surbtc->getLastError());
+}
+
---
+++ examples/order_book.php
@@ -0,0 +1,20 @@
+<?php
+//1. Include Surbtc library
+require_once '../Surbtc.php';
+
+//2. Create Surbtc object
+$surbtc = new SurBTC\Surbtc();
+
+//2.1 activate debug mode
+//$surbtc->debug(true);
+
+//3. Call method to get order book
+$response = $surbtc->orderBook('eth-btc');
+
+//4. Check response
+if(false != $response)
+  print_r($response);
+else{
+  print_r($surbtc->getLastError());
+}
+
